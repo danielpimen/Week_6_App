24a8b85c9bd618130835c48c403da1c418b348f1
Daniel P <danielpimen92@gmail.com>
2017-12-29 23:46:17 -0500

    rough draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,38 @@
-console.log('Hello');
+var buttonList = ['dog', 'cats', 'hedgehog', 'pig']
+var input;
+
+
+function createButton() {
+    $('.buttonHolder').empty();
+    for (var i = 0; i < buttonList.length; i++) {
+        var newButton = $('<button>');
+        newButton.addClass('searchWord');
+        newButton.attr('data-name', buttonList[i]);
+        newButton.text(buttonList[i]);
+
+        $('.buttonHolder').append(newButton);
+    }
+}
+$('#add-word').on('click', function(event) {
+    event.preventDefault();
+    var input = $('#user-input').val().trim();
+    buttonList.push(input);
+    console.log(buttonList);
+    createButton()
+})
+createButton();
+function displayInfo(){
+	var userSearch=$(this).attr('data-name');
+	var queryURL = "https://www.omdbapi.com/?t=" + userSearch + "&y=&plot=short&apikey=40e9cece";
+
+$.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+          console.log(response)});
+console.log('hello')
+}
+
+
+
+$(document).on("click", ".searchWord", displayInfo);
