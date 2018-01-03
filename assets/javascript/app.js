8ab14dd6c0cccda7af35f110603caa7297df4ebf
Daniel P <danielpimen92@gmail.com>
2018-01-03 13:28:06 -0500

    progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,20 +21,30 @@ $('#add-word').on('click', function(event) {
     createButton()
 })
 createButton();
-function displayInfo(){
-	var userSearch=$(this).attr('data-name');
-	var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=8R7qN3TiJmJOpkLz628uGBYZ8aMsXgkV&q=' + userSearch + '&limit=25&offset=0&rating=G&lang=en';
 
-$.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response) {
-          console.log(response)
+function displayInfo() {
+    var userSearch = $(this).attr('data-name');
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=8R7qN3TiJmJOpkLz628uGBYZ8aMsXgkV&q=' + userSearch + '&limit=10&offset=0&rating=G&lang=en';
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+        console.log(response)
+       // var results = response.data;
+        //console.log(results)
+        for (var i = 0; i < response.length; i++) {
+            //grab gif
+            println(response.response[i].images.original.url);
+            
+        }
 
 
 
-$('.resultsDiv').text('Sup Hoe')
-})}
+
+      //  $('.resultsDiv').append(gif);
+    })
+}
 
 
 $(document).on("click", ".searchWord", displayInfo);
