108e0e58794da9fe01d81fefe3a5a4db50163bf1
Daniel P <danielpimen92@gmail.com>
2018-01-04 20:39:38 -0500

    animated gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,10 @@
+//App Instructions: User inputs data, data is turned into a button. When the button is clicked, API is called and 10 images are displayed.
+
+//Button List
 var buttonList = ['dog', 'cats', 'hedgehog', 'pig']
 var input;
 
-
+// Create Button From Array List 
 function createButton() {
     $('.buttonHolder').empty();
     for (var i = 0; i < buttonList.length; i++) {
@@ -14,6 +17,7 @@ function createButton() {
     }
 }
 
+//Push User Input Into Button Array
 $('#add-word').on('click', function(event) {
     event.preventDefault();
     var input = $('#user-input').val().trim();
@@ -23,6 +27,7 @@ $('#add-word').on('click', function(event) {
 })
 createButton();
 
+//Fetch GIFs From GIPHY API 
 function displayInfo() {
     var userSearch = $(this).attr('data-name');
     var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=8R7qN3TiJmJOpkLz628uGBYZ8aMsXgkV&q=' + userSearch + '&limit=10&offset=0&rating=G&lang=en';
@@ -37,7 +42,11 @@ function displayInfo() {
         for (var i = 0; i < results.length; i++) {
             var gifDiv = $("<div class='item'>");
             var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height.url);
+            personImage.attr("src", results[i].images.fixed_height_still.url);
+            personImage.attr('data-still', results[i].images.fixed_height_still.url)
+            personImage.attr('data-animate', results[i].images.fixed_height.url)
+            personImage.attr('data-state', 'still')
+            personImage.addClass('gifClass')
             gifDiv.prepend(personImage);
             $(".resultsDiv").prepend(gifDiv);
         }
@@ -45,9 +54,20 @@ function displayInfo() {
 
 
 
-        //  $('.resultsDiv').append(gif);
+
+
     })
+
 }
+//On click of image, turn still GIF to moving GIF
+$(document).on('click', '.gifClass', function moveGif(){
+    var state = $(this).attr('data-state'); 
+    if (state == 'still'){
+        $(this).attr('src', $(this).data('animate'));
+        $(this).attr('data-state', 'animate');
 
+    }
+})
 
+//Execute on click of button 
 $(document).on("click", ".searchWord", displayInfo);
